62bd733bca8a38b4b419bb165f077c2bc6008476
filippobartolucci <filippo.bartolucci2@studio.unibo.it>
2022-12-20 08:39:26 +0100

    csv

---
+++ toCSV.py
@@ -0,0 +1,23 @@
+import csv
+
+file = "./TestSet_EAGLES.goldS.OFFICIAL"
+output = "out.csv"
+
+
+# read the file
+with open(file, 'r') as f:
+    lines = f.readlines()
+
+    # clear output file
+    with open(output, 'w') as o:
+        # write the header
+        writer = csv.writer(o, delimiter='\t')
+        writer.writerow(["word", "tag", "lem"])
+
+        for l in lines:
+            # write the list to a csv file
+            with open(output, 'a') as o:
+                writer.writerow(l.split())
+
+        
+    
